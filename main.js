b39ee048f5978a73cd7b6bb3f3dbcd6baac7388d
Mozgur <dfaunm@gmail.com>
2022-06-03 12:34:13 +0300

    Coded the gameControllerModule for manipulating game state

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ const GameFactory = () => {
     }
   };
 
-  const getSign = (index){
+  const getSign = (index) => {
     if (!(index > board.length)) {
       return board[index];
     }
-  }
+  };
 
   const reset = () => {
     for (let i = 0; i < 9; i++) {
@@ -29,4 +29,71 @@ const PlayerFactory = (player_sign) => {
   return { getSign };
 };
 
-const initializeGame = (() => {})();
+const pageControllerModule = (() => {})();
+
+const gameControllerModule = (() => {
+  const game = GameFactory();
+  const PlayerX = PlayerFactory("X");
+  const PlayerO = PlayerFactory("O");
+  let turnCount = 1;
+  let isOver = false;
+
+  const play = (index) => {
+    game.setSign(
+      index,
+      turnCount % 2 == 0 ? PlayerO.getSign() : PlayerX.getSign()
+    );
+    // TODO: check win
+    if (winCheck(index)) {
+      isOver = true;
+      // TODO: change the p element inside the html to win
+
+      return;
+    } else {
+      if (turnCount == 9) {
+        isOver = true;
+        // change p element inside html to draw
+      } else {
+        turnCount++;
+        // change p element inside html to next player
+      }
+    }
+  };
+  const getTurnCount = () => {
+    return turnCount;
+  };
+
+  const resetGame = () => {
+    turnCount = 1;
+    isOver = false;
+    game.reset();
+  };
+
+  const getIsOver = () => {
+    return isOver;
+  };
+
+  const winCheck = (clickedIndex) => {
+    winConds = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    return winConds
+      .filter((possibleCond) => {
+        possibleCond.includes(clickedIndex);
+      })
+      .some((possibleCond2) => {
+        possibleCond2.every((index) => {
+          game.getSign(index) ===
+            (turnCount % 2 == 0 ? PlayerO.getSign() : PlayerX.getSign());
+        });
+      });
+  };
+  return { play, getIsOver, resetGame };
+})();
